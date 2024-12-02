ca14a044562e7386e42c080e6c17a36a7595f381
migDuk <143349960+Dukzf1v@users.noreply.github.com>
2024-12-02 14:38:56 +0700

    remove OtherProjects

--- OtherProjects/hust/soict/dsai/Lab01/hust/soict/dsai/Lab01/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package hust.soict.dsai.Lab01;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Xin chao \ncac ban!"); 
-        System.out.println("Hello \tworld!");      
-    } 
-}
