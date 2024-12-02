03f6b1a3a5af427d5198b361662c2f3a28e2216d
migDuk <143349960+Dukzf1v@users.noreply.github.com>
2024-12-02 14:31:36 +0700

    Lab04

---
+++ OtherProjects/hust/soict/dsai/Lab01/hust/soict/dsai/Lab01/FirstDialog.java
@@ -0,0 +1,11 @@
+package hust.soict.dsai.Lab01;
+
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        // Display a message dialog
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0); // Exit the program
+    }
+}
